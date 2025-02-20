45dbe58d9c30cac080a79393b2bccfbdb777597d
canfei <canfeit@qq.com>
2025-02-20 21:19:33 +0800

    feat

--- scripts.js
+++ scripts.js
@@ -1,24 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const t = document.getElementById("copyrightYear");
-  t && (t.textContent = (new Date).getFullYear(), t.setAttribute("datetime", t.textContent))
+  t &&
+    (t.textContent = (new Date()).getFullYear(),
+      t.setAttribute("datetime", t.textContent));
 });
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', async () => {
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
     try {
-      await navigator.serviceWorker.register('sw.js');
+      await navigator.serviceWorker.register("sw.js");
     } catch (error) {
-      console.error('ServiceWorker注册失败：', error);
+      console.error("ServiceWorker注册失败：", error);
     }
   });
 }
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
     e.preventDefault();
 
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
   });
 });
